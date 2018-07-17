21aa0bde3b67b20a609928d13ab9c6aedd907374
ChelseaYuChen <x236892001@gmail.com>
2018-07-17 17:16:15 +0800

    ColourNote

---
+++ app/src/main/java/com/byethost15/omgyimin/myapplication/ItemData.java
@@ -0,0 +1,17 @@
+package com.byethost15.omgyimin.myapplication;
+
+public class ItemData {
+
+    String name;
+    String code;
+
+    public ItemData(String name,String code){
+
+        this.name=name;
+        this.code=code;
+    }
+
+    public String getName(){return name;}
+    public String getCode(){return code;}
+
+}
